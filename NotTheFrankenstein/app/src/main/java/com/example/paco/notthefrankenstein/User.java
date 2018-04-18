b319ab5ab2afbb62eea479a035fa91838c2a8060
kmazas <kevin.mazas.528@my.csun.edu>
2018-04-18 15:48:32 -0700

    Revert "Merge branch 'ActivitiesReworkBranch' into SettingsBranch"
    This reverts commit 8183915ccdbd0b99e18bb930f9f293c592dd155f, reversing
    changes made to 553f3e248205db9114bf19e96e70575606d79822.

--- NotTheFrankenstein/app/src/main/java/com/example/paco/notthefrankenstein/User.java
+++ NotTheFrankenstein/app/src/main/java/com/example/paco/notthefrankenstein/User.java
@@ -4,39 +4,15 @@ package com.example.paco.notthefrankenstein;
 public class User {
     String user_name;
     String email;
-    String uid;
+    String Uid;
 
     public User (){
         //default constructor
     }
 
-    public User(String user_name, String email, String uid){
-        setUser_name(user_name);
-        setEmail(email);
-        setUid(uid);
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public void setUser_name(String user_name) {
+    public User(String user_name, String email, String Uid){
         this.user_name = user_name;
+        this.email = email;
+        this.Uid = Uid;
     }
 }
